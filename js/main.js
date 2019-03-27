604fa62d08fb6a1fa0d7f37c2c2e0c6deb3f464f
Dewanshu Pratihar <dewanshu@Dewanshus-MacBook-Pro.local>
2019-03-27 16:12:26 +0530

    Updates

--- js/main.js
+++ js/main.js
@@ -4,11 +4,30 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const tabSwitch = document.querySelectorAll(".w3-button");
 
 //Set Initial State of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+
+tabSwitch.forEach(function(tab) {
+  tab.addEventListener("click", function() {
+    var active = tab.classList.contains("current");
+    if (!active) tab.classList.add("current");
+    var appear = tab.id + "-projects";
+    document.getElementsByClassName(appear)[0].style.display = "grid";
+
+    tabSwitch.forEach(othertab => {
+      if (othertab != tab) {
+        othertab.classList.remove("current");
+        var disappear = othertab.id + "-projects";
+        console.log(disappear);
+        document.getElementsByClassName(disappear)[0].style.display = "none";
+      }
+    });
+  });
+});
 
 function toggleMenu() {
   if (!showMenu) {
@@ -17,6 +36,8 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
+
+    console.log("run");
 
     //Set Menu State
     showMenu = true;
